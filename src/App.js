c141377bca94344804534fdaf9e7481049203265
Kim Thompson <kthompson@mpr.org>
2019-01-26 13:58:51 -0600

    basic homepage and other petty formatting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
+import HomePage from './pages/HomePage'
 import './styles/App.scss'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1 className='hdg hdg-1 hdg-main'>Look, a header!</h1>
-        <p>Wow, it's so beautiful</p>
+        <HomePage />
       </div>
     );
   }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className='homePage'>
+        <h1 className='hdg hdg-1 hdg-main'>Look, a header!</h1>
+        <p>Wow, it's so beautiful</p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;

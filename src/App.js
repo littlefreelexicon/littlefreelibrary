051c99b5d3716ef545a58a22b5cea415bbb01d0a
Sarah Rose Battles <srosebattles@gmail.com>
2019-01-26 19:18:23 -0600

    changed a route

--- src/App.js
+++ src/App.js
@@ -3,20 +3,6 @@ import HomePage from './pages/HomePage'
 import './styles/App.scss'
 
 class App extends Component {
-  state = {
-    books: [],
-  }
-
-  componentDidMount() {
-  this.getBooksFromDb();
-}
-
-getBooksFromDb = () => {
-  fetch("http://localhost:3000/api/getAllBooks")
-    .then(data => data.json())
-    .then(res => this.setState({ data: res.data }));
-};
-
   render() {
     return (
       <div className='app'>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,6 +5,20 @@ import BookCard from '../components/BookCard'
 import CardHolder from '../components/CardHolder'
 
 class HomePage extends Component {
+  state = {
+    books: [],
+  }
+
+componentDidMount() {
+  this.getBooksFromDb();
+}
+
+getBooksFromDb = () => {
+  fetch("http://localhost:3001/api/getAllBooks")
+    .then(data => data.json())
+    .then(res => this.setState({ data: res.data }));
+};
+
   render() {
     return (
       <React.Fragment>

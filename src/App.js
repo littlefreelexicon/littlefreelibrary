77c8345cc0727253fad8d9830bf85e285235b248
Sarah Rose Battles <srosebattles@gmail.com>
2019-01-26 18:20:23 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import HomePage from './pages/HomePage'
 import './styles/App.scss'
 
 class App extends Component {
+  state = {
+    book: {}
+  }
+
+  componentDidMount() {
+  this.getBookFromDb();
+}
+
+getBookFromDb = () => {
+  fetch("http://localhost:3001/api/getData")
+    .then(data => data.json())
+    .then(res => this.setState({ data: res.data }));
+};
+
   render() {
     return (
       <div className='App'>

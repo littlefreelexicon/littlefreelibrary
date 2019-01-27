01ee1a7af2aedd463631e7fffda4b1ddea64a47b
Christiana <christiana.routon@gmail.com>
2019-01-27 10:27:05 -0500

    search area view done

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
+import SearchArea from '../components/SearchArea'
 import CardHolder from '../components/CardHolder'
 
 class HomePage extends Component {
@@ -10,6 +11,7 @@ class HomePage extends Component {
         <Header />
         <Search />
         <CardHolder />
+        <SearchArea />
       </React.Fragment>
     )
   }

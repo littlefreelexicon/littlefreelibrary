eaa6aa48c4168d19871f105db68ddec98443b57d
Madeleine Filloux <mfilloux@gmail.com>
2019-01-27 11:17:14 -0600

    fix some styling issues. get library page image to match book detail page

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -19,7 +19,6 @@ class BookDetail extends Component {
     fetch(`http://localhost:3001/bookDetail/${id}`)
       .then(data => data.json())
       .then(res => {
-        console.log(res)
         this.setState({ book: res })
       })
   }
@@ -44,14 +43,19 @@ class BookDetail extends Component {
             alt=''
             src={this.state.book.library && this.state.book.library.photo}/>
           <div className='library-address'>
+          <h3>{this.state.book.library && this.state.book.library.libraryName}</h3>
             <a href={this.state.book.library && `http://localhost:3000/librarydetails/${this.state.book.library._id}`}>
                 <FontAwesomeIcon icon={faMapMarkerAlt} />
-                {this.state.book.library && this.state.book.library.location.address}
+                <span>
+                {this.state.book.library && " " + this.state.book.library.location.address.split(", ")[0]}
+                <br></br>
+                {this.state.book.library && this.state.book.library.location.address.split(", ")[1]}
+                </span>
             </a>
           </div>
+        </div>
         <span><button className='get-directions'>Get Directions</button></span>
         <span><button className='check-out'>Check out this book</button></span>
-        </div>
       </div>
     );
   }

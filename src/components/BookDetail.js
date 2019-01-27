27ab5fbd79f011c0869364345e34f11a54e7a511
Madeleine Filloux <mfilloux@gmail.com>
2019-01-27 09:37:33 -0600

    improved book detail styling

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkerAlt, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 class BookDetail extends Component {
   constructor (props) {
@@ -19,6 +19,7 @@ class BookDetail extends Component {
     fetch(`http://localhost:3001/bookDetail/${id}`)
       .then(data => data.json())
       .then(res => {
+        console.log(res)
         this.setState({ book: res })
       })
   }
@@ -27,31 +28,27 @@ class BookDetail extends Component {
     console.log(this.state)
     return (
       <div className='book-detail-page'>
+        <FontAwesomeIcon icon={faArrowLeft} />
         <img
           className='book-detail-main-image'
-          alt=''
-          src='https://www.w3schools.com/images/picture.jpg'/>
+          src={this.state.book.genres ? (process.env.PUBLIC_URL + '/images/' + this.state.book.genres[0] + '.svg') : (process.env.PUBLIC_URL + '/images/fiction.svg')}
+          alt="book genre icon" />
         <span className="book-title">
           <h2>{this.state.book.title}</h2>
-          <h4>By: {this.state.book.author}</h4>
+          <h4>By {this.state.book.author}</h4>
         </span>
-        <h2 className="description">Location</h2>
+        <h3 className="description">Find it here:</h3>
         <div className='address-border'>
           <img
             className='book-detail-library-image'
             alt=''
-            src='https://www.w3schools.com/images/picture.jpg'/>
-          <span className='library-address'>
-            <a href='/' className='icon-map-marker'>
-              <FontAwesomeIcon icon={faMapMarkerAlt} />
-            </a><br />
-            <a href='/'>{this.state.book.library && this.state.book.library.location.address}</a>
-          </span>
+            src={this.state.book.library && this.state.book.library.photo}/>
+          <div className='library-address'>
+            <a href='/'><FontAwesomeIcon icon={faMapMarkerAlt} /> {this.state.book.library && this.state.book.library.location.address}</a>
+          </div>
         </div>
         <span><button className='get-directions'>Get Directions</button></span>
         <span><button className='check-out'>Check out this book</button></span>
-        <h2 className='description'>Description</h2>
-        <p className='description'>{this.state.book.title}</p>
       </div>
     );
   }

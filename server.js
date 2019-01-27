b2842a347582ea13ca14aeec63d968b942b5f30f
Madeleine Filloux <mfilloux@gmail.com>
2019-01-26 19:35:49 -0600

    added back in routes require to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const env = require('dotenv').load()
+const routes = require('./routes.js')
 
 const PORT = process.env.PORT || 3001
 const app = express()

4ccd8c49f790eb0168bea9e7db323a43e943ea20
Ana Knickerbocker <ana.knickerbocker@gmail.com>
2019-02-07 10:04:49 -0600

    removed /build from .gitignore

---
+++ build/precache-manifest.2fe5e17e2e0b86c19dd07567a2ff11d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3db1f66b20e776c76fd5",
+    "url": "/static/js/main.3db1f66b.chunk.js"
+  },
+  {
+    "revision": "48dffb98121554e6b1d6",
+    "url": "/static/js/1.48dffb98.chunk.js"
+  },
+  {
+    "revision": "3db1f66b20e776c76fd5",
+    "url": "/static/css/main.9bdb416c.chunk.css"
+  },
+  {
+    "revision": "53cb739b2a117c2d36bb4e0bb62aefb4",
+    "url": "/index.html"
+  }
+];
